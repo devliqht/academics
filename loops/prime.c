718d4bebb50c42a592084090b529280a22723df4
Matt Cabarrubias <monapwc2020@gmail.com>
2024-11-06 20:06:08 +0800

    Wowow

--- loops/prime.c
+++ loops/prime.c
@@ -1,23 +1,23 @@
-#include <stdio.h>
+    #include <stdio.h>
 
-int main(void) {
-    int num;
+    int main(void) {
+        int num;
 
-    printf("Enter range (from 1 to): ");
-    scanf("%d",&num);
+        printf("Enter range (from 1 to): ");
+        scanf("%d",&num);
 
-    for (int i = 1; i <= num; i++) {
-        int count = 0;
-        printf("Number [%d]: \n", i);
-        for (int j = 1; j <= num; j++) {
-            if (i % j == 0) {
-                printf("\tFactor: %d\n", j);
-                count++;
+        for (int i = 1; i <= num; i++) {
+            int count = 0;
+            printf("Number [%d]: \n", i);
+            for (int j = 1; j <= num; j++) {
+                if (i % j == 0) {
+                    printf("\tFactor: %d\n", j);
+                    count++;
+                }
+            }
+            if (count == 2) {
+                printf("\tThis number is a prime number. \n");
             }
         }
-        if (count == 2) {
-            printf("\tThis number is a prime number. \n");
-        }
+        return 0;
     }
-    return 0;
-}
