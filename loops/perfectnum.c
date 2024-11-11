1c0661b4d20317bf595e838c0ff5cad40e8a58d8
Matt Cabarrubias <monapwc2020@gmail.com>
2024-11-11 18:37:44 +0800

    more

--- loops/perfectnum.c
+++ loops/perfectnum.c
@@ -4,10 +4,11 @@ int main() {
     int num;
     printf("Enter the value of n: "); scanf("%d", &num);
     printf("Perfect numbers up to %d:\n", num);
+
     for (int i = 1; i <= num; i++) { // 6
         int sum = 0;
         for (int j = 1; j < i; j++) { 
-            if (i % j == 0) { 
+            if (i % j == 0) {  // ex: 6; 6 % 1, 6 % 2, 6 % 3 = 1 + 3 + 2
                 sum += j;
             }
         }
