3ff4e19614b8a2b85cf92406b527a66bfb7e7107
Matt Cabarrubias <monapwc2020@gmail.com>
2024-11-11 21:53:24 +0800

    refactor

--- arrays/matrix/max-element.c
+++ arrays/matrix/max-element.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define MAX 50
 
 /*
     Enter the number of rows: 5
@@ -11,3 +12,25 @@
     11 12 13 14 15
     Maximum element in the array is: 15
 */
+
+int main(void) {
+    int row, col, arr[MAX][MAX] = {{0}};
+    
+    printf("Enter the number of rows: "); scanf("%d", &row);
+    printf("Enter the number of columns: "); scanf("%d", &col);
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    int max = arr[0][0];
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            if (arr[i][j] > max) {
+                max = arr[i][j];
+            }
+        }
+    }
+    printf("Maximum element in the array is: %d", max);
+}
